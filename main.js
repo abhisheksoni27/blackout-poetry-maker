67ae6e5de6d341d7878e357a885e981a53f714c3
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-06-01 00:42:55 +0530

    draw rectangles

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 window.onload = () => {
-    canvas.width = window.innerWidth; //horizontal resolution (?) - increase for better looking text
-    canvas.height = window.innerHeight; //vertical resolution (?) - increase for better looking text
+
+    var mouseIsDown = false;
+    var startX;
+    var startY;
+    var endX;
+    var endY;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     const ctx = canvas.getContext('2d');
     const image = new Image();
 
-    image.onload = ()=>{
-        ctx.drawImage(image, 0, 0)
+    image.onload = () => {
+        ctx.drawImage(image, 0, 0);
     }
 
     image.src = "page.png"
-        
+
+    window.onmousedown = function (e) {
+        console.log(e)
+        mouseIsDown = true;
+        startX = e.pageX - canvas.offsetLeft;
+        startY = e.pageY - canvas.offsetTop;
+        canvas.style.cursor = "crosshair";
+    }
+
+    window.onmouseup = function (e) {
+        endX = e.pageX - canvas.offsetLeft;
+        endY = e.pageY - canvas.offsetTop;
+        ctx.strokeStyle = 'red';
+        ctx.rect(startX, startY, endX - startX, endY - startY);
+        ctx.stroke();
+        canvas.style.cursor = "default";
+    }
+
 }
