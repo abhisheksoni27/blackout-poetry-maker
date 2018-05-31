02bfd342e7af19dd7f89aa829639da8d90edd6f4
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-05-31 23:45:15 +0530

    load image

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 window.onload = () => {
-    const context = canvas.getContext('2d')
+    canvas.width = window.innerWidth; //horizontal resolution (?) - increase for better looking text
+    canvas.height = window.innerHeight; //vertical resolution (?) - increase for better looking text
+
+    const ctx = canvas.getContext('2d');
+    const image = new Image();
+
+    image.onload = ()=>{
+        ctx.drawImage(image, 0, 0)
+    }
+
+    image.src = "page.png"
+        
 }
