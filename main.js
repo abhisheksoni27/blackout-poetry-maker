e18ff664b88d7eaf6f4628cf2c848b656abf2444
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-06-01 01:00:13 +0530

    boilerplate

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
+let mouseIsDown = false;
+let startX;
+let startY;
+let endX;
+let endY;
+const image = new Image();
+const rectangles  = [];
+
 window.onload = () => {
-
-    var mouseIsDown = false;
-    var startX;
-    var startY;
-    var endX;
-    var endY;
-
+    create.onclick = makeBlackoutPoetry;
     canvas.width = window.innerWidth;
+
     canvas.height = window.innerHeight;
 
     const ctx = canvas.getContext('2d');
-    const image = new Image();
 
     image.onload = () => {
-        ctx.drawImage(image, 0, 0);
+        ctx.drawImage(image, canvas.width / 2 - image.width / 2, canvas.height / 2 - image.height / 2);
     }
 
     image.src = "page.png"
 
     window.onmousedown = function (e) {
-        console.log(e)
+        canvas.style.cursor = "crosshair";
         mouseIsDown = true;
         startX = e.pageX - canvas.offsetLeft;
         startY = e.pageY - canvas.offsetTop;
-        canvas.style.cursor = "crosshair";
     }
 
     window.onmouseup = function (e) {
+        canvas.style.cursor = "default";
+        mouseIsDown = false;
         endX = e.pageX - canvas.offsetLeft;
         endY = e.pageY - canvas.offsetTop;
-        ctx.strokeStyle = 'red';
-        ctx.rect(startX, startY, endX - startX, endY - startY);
-        ctx.stroke();
-        canvas.style.cursor = "default";
-    }
+        const rect = {
+            x: startX,
+            y: startY,
+            width: endX - startX,
+            height: endY - startY
+        }
 
+        drawRectangle(ctx, rect.x, rect.y, rect.width, rect.height);
+
+        rectangles.push(rect);
+    }
+}
+
+function makeBlackoutPoetry(){
+
+}
+
+function drawRectangle(ctx, x, y, width, height) {
+    ctx.strokeStyle = 'red';
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
 }
